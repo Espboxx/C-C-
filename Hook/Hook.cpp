e02a6b69a7ee1d293ebbe7391e3ff362f4484f25
jiangbo <937158168@qq.com>
2020-06-23 10:54:10 +0800

    Add files via upload

---
+++ Hook/Hook.cpp
@@ -0,0 +1,122 @@
+#include <d3d11.h>
+#include <iostream>
+#pragma comment(lib, "d3d11.lib")
+DWORD_PTR* pSwapChainVtable = NULL;
+DWORD_PTR* pContextVTable = NULL;
+DWORD_PTR* pDeviceVTable = NULL;
+ID3D11Device *pDevice = NULL;
+ID3D11DeviceContext *pContext = NULL;
+const int MultisampleCount = 1;
+LRESULT CALLBACK DXGIMsgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) { return DefWindowProc(hwnd, uMsg, wParam, lParam); }
+
+DWORD __stdcall InitializeHook(LPVOID)
+{
+	//Sleep(5000);
+
+	HMODULE hDXGIDLL = 0;
+	do
+	{
+		hDXGIDLL = GetModuleHandleA ("dxgi.dll");
+		Sleep(80);
+	} while (!hDXGIDLL);
+	Sleep(100);
+	IDXGISwapChain* pSwapChain;
+	WNDCLASSEXA wc = { sizeof(WNDCLASSEX), CS_CLASSDC, DXGIMsgProc, 0L, 0L, GetModuleHandleA(NULL), NULL, NULL, NULL, NULL, "DX", NULL };
+	RegisterClassExA(&wc);
+	HWND hWnd = CreateWindowA("DX", NULL, WS_OVERLAPPEDWINDOW, 100, 100, 300, 300, NULL, NULL, wc.hInstance, NULL);
+
+	D3D_FEATURE_LEVEL requestedLevels[] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_1 };
+	D3D_FEATURE_LEVEL obtainedLevel;
+	ID3D11Device* d3dDevice = nullptr;
+	ID3D11DeviceContext* d3dContext = nullptr;
+
+	DXGI_SWAP_CHAIN_DESC scd;
+	ZeroMemory(&scd, sizeof(scd));
+	scd.BufferCount = 1;
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	scd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	scd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+
+	scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	scd.OutputWindow = hWnd;
+	scd.SampleDesc.Count = MultisampleCount;
+	scd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	scd.Windowed = ((GetWindowLongPtr(hWnd, GWL_STYLE) & WS_POPUP) != 0) ? false : true;
+
+	scd.BufferDesc.Width = 1;
+	scd.BufferDesc.Height = 1;
+	scd.BufferDesc.RefreshRate.Numerator = 0;
+	scd.BufferDesc.RefreshRate.Denominator = 1;
+
+	UINT createFlags = 0;
+	IDXGISwapChain* d3dSwapChain = 0;
+
+	if (FAILED(D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		createFlags,
+		requestedLevels,
+		sizeof(requestedLevels) / sizeof(D3D_FEATURE_LEVEL),
+		D3D11_SDK_VERSION,
+		&scd,
+		&pSwapChain,
+		&pDevice,
+		&obtainedLevel,
+		&pContext)))
+	{
+		MessageBoxA (hWnd, "Failed to create directX device and swapchain!", "Error", MB_ICONERROR);
+		return NULL;
+	}
+
+	pSwapChainVtable = (DWORD_PTR*)pSwapChain;
+	pSwapChainVtable = (DWORD_PTR*)pSwapChainVtable[0];
+
+	pContextVTable = (DWORD_PTR*)pContext;
+	pContextVTable = (DWORD_PTR*)pContextVTable[0];
+
+	pDeviceVTable = (DWORD_PTR*)pDevice;
+	pDeviceVTable = (DWORD_PTR*)pDeviceVTable[0];
+
+	DWORD hmap;
+	DWORD hevent;
+	DWORD pbuffer;
+	hmap = (DWORD)CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, 256, "Trans");
+	if (hmap != NULL)
+	{
+		pbuffer = (DWORD)MapViewOfFile ((HANDLE)hmap, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+	}
+
+
+	DWORD TSwapChainVtable;
+	TSwapChainVtable = pbuffer + 30;
+
+	*(DWORD*)TSwapChainVtable = (DWORD)pSwapChainVtable;
+
+	DWORD TContextVTable;
+	TContextVTable = pbuffer + 40;
+	*(DWORD*)TContextVTable = (DWORD)pContextVTable;
+
+	DWORD TDeviceVTable;
+	TDeviceVTable = pbuffer + 50;
+	*(DWORD*)TDeviceVTable = (DWORD)pDeviceVTable;
+
+}
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hModule);
+		CreateThread(NULL, NULL, reinterpret_cast<LPTHREAD_START_ROUTINE>(InitializeHook), NULL, NULL, NULL);
+		break;
+
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
